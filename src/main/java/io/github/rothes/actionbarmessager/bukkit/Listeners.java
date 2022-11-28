378b8434df136c7c5e2fa766548ff3301cb5e734
Rothes <449181985@qq.com>
2022-11-28 21:25:19 +0800

    final

--- src/main/java/io/github/rothes/actionbarmessager/bukkit/Listeners.java
+++ src/main/java/io/github/rothes/actionbarmessager/bukkit/Listeners.java
@@ -5,7 +5,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 
-public class Listeners implements Listener {
+public final class Listeners implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerChangeWorld(PlayerChangedWorldEvent event) {
