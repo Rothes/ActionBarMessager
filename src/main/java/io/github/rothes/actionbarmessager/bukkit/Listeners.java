7a75297fbc184b66e9b73b7ad0bafae4e8ba3e7e
Rothes <449181985@qq.com>
2023-01-29 21:39:57 +0800

    Update messages on join

--- src/main/java/io/github/rothes/actionbarmessager/bukkit/Listeners.java
+++ src/main/java/io/github/rothes/actionbarmessager/bukkit/Listeners.java
@@ -4,11 +4,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public final class Listeners implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerChangeWorld(PlayerChangedWorldEvent event) {
+        ActionBarMessager.getInstance().getUserManager().getUser(event.getPlayer()).updateWorldMessages();
+    }
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerJoin(PlayerJoinEvent event) {
         ActionBarMessager.getInstance().getUserManager().getUser(event.getPlayer()).updateWorldMessages();
     }
 
